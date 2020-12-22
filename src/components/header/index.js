ad4bf9fb12c8ca9f9c897798431cc94fedef19a1
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-12-22 20:05:37 +0100

    first commit in part one without test

---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="wrapper">
+        <div className="logo">
+          LOGO
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
